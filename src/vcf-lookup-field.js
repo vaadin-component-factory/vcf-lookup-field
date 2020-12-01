cb52887116649807137cc7e5f80d11fc59512054
Jean-Christophe Gueriaud <jean-christophe@vaadin.com>
2020-12-01 11:52:58 +0200

    v1.0.6

--- src/vcf-lookup-field.js
+++ src/vcf-lookup-field.js
@@ -271,7 +271,7 @@ class VcfLookupField extends ElementMixin(ThemableMixin(PolymerElement)) {
   }
 
   static get version() {
-    return '1.0.5';
+    return '1.0.6';
   }
 
   static get properties() {
