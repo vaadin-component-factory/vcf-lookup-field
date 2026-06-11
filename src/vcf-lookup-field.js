35f320f5ef9deda6900a81e77494189feedd7901
Jean-Christophe Gueriaud <51313578+jcgueriaud1@users.noreply.github.com>
2026-06-11 10:49:52 +0300

    Cherry pick issue 30 (#25)
    * v5.1.3

--- src/vcf-lookup-field.js
+++ src/vcf-lookup-field.js
@@ -554,7 +554,7 @@ export class LookupField extends ElementMixin(ThemableMixin(PolymerElement)) {
   }
 
   static get version() {
-    return '5.0.1';
+    return '5.1.3';
   }
 
   static get properties() {
